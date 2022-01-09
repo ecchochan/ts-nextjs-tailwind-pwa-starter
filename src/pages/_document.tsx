5945019f2752d24b653b1d8de8e3e6eb5f6a9303
Echo Chan <echo.chan@9gag.com>
2022-01-09 11:13:30 +0800

    feat: add CSP

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+import crypto from 'crypto';
 import Document, {
   DocumentContext,
   Head,
@@ -5,6 +6,34 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document';
+import { v4 } from 'uuid';
+
+/**
+ * Generate Content Security Policy for the app.
+ * Uses randomly generated nonce (base64)
+ *
+ * @returns [csp: string, nonce: string] - CSP string in first array element, nonce in the second array element.
+ */
+const generateCsp = (): [csp: string, nonce: string] => {
+  const production = process.env.NODE_ENV === 'production';
+
+  // generate random nonce converted to base64. Must be different on every HTTP page load
+  const hash = crypto.createHash('sha256');
+  hash.update(v4());
+  const nonce = hash.digest('base64');
+
+  let csp = ``;
+  csp += `default-src 'self';`;
+  csp += `base-uri 'self';`;
+  csp += `style-src 'self' https://fonts.googleapis.com 'unsafe-inline';`; // NextJS requires 'unsafe-inline'
+  csp += `script-src 'nonce-${nonce}' 'self' ${
+    production ? 'unsafe-inline' : "'unsafe-eval'"
+  };`; // NextJS requires 'self' and 'unsafe-eval' in dev (faster source maps)
+  csp += `font-src 'self' https://fonts.gstatic.com;`;
+  if (!production) csp += `connect-src 'self';`;
+
+  return [csp, nonce];
+};
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -13,9 +42,12 @@ class MyDocument extends Document {
   }
 
   render() {
+    const [csp, nonce] = generateCsp();
     return (
       <Html lang='en'>
-        <Head>
+        <Head nonce={nonce}>
+          <meta property='csp-nonce' content={nonce} />
+          <meta httpEquiv='Content-Security-Policy' content={csp} />
           <link
             rel='preload'
             href='/fonts/inter-var-latin.woff2'
@@ -26,7 +58,7 @@ class MyDocument extends Document {
         </Head>
         <body>
           <Main />
-          <NextScript />
+          <NextScript nonce={nonce} />
         </body>
       </Html>
     );
