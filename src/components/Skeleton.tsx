d62f07355889d83e23423719443970a45636849a
Echo Chan <echo.chan@9gag.com>
2022-03-06 20:13:11 +0800

    feat: skeleton add dark mode

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -2,15 +2,22 @@ import * as React from 'react';
 
 import clsxm from '@/lib/clsxm';
 
+import { useDarkMode } from '@/store/app';
+
 type SkeletonProps = React.ComponentPropsWithoutRef<'div'>;
 
 export default function Skeleton({ className, ...rest }: SkeletonProps) {
+  const darkMode = useDarkMode()[0];
   return (
     <div
-      className={clsxm('animate-shimmer bg-[#f6f7f8]', className)}
+      className={clsxm(
+        'animate-shimmer bg-[#f6f7f8] dark:bg-[#202020]',
+        className
+      )}
       style={{
-        backgroundImage:
-          'linear-gradient(to right, #f6f7f8 0%, #edeef1 20%, #f6f7f8 40%, #f6f7f8 100%)',
+        backgroundImage: darkMode
+          ? 'linear-gradient(to right, #202020 0%, #3b3b3b 20%, #202020 40%, #202020 100%)'
+          : 'linear-gradient(to right, #f6f7f8 0%, #edeef1 20%, #f6f7f8 40%, #f6f7f8 100%)',
         backgroundSize: '700px 100%',
         backgroundRepeat: 'no-repeat',
       }}
